071ad0a13329edd75dcb059453930b818c365b20
Kelia Simbi <simbikelia@gmail.com>
2026-02-25 10:30:27 +0200

    import eexisting schemas

---
+++ studio-ipac/schemaTypes/index.ts
@@ -0,0 +1,7 @@
+import blog from './blog'
+import gallery from './gallery'
+import project from './project'
+import overview from './overview'
+import workshop from './workshop'
+
+export const schemaTypes = [blog, gallery, project, overview, workshop]
